f46e639db1e36e90282c2d00984255011c9cb47b
spencerpenn <spenn321@gmail.com>
2014-04-29 02:27:24 -0400

    Updated downsampling

--- downsampleGlove.m
+++ downsampleGlove.m
@@ -1,9 +1,10 @@
 % Downsampling dataglove traces
 
-function[ subj1TrainingGlove_down ] = downsampleGlove (subj1TrainingGlove)
+function[subj1TrainingGlove_down] = downsampleGlove (subj1TrainingGlove)
 
+lengthData = length(subj1TrainingGlove);
 r = 50;
-subj1TrainingGlove_down = NaN(5,400000/r);
+subj1TrainingGlove_down = NaN(5,lengthData/r);
 
 for i = 1:5
     subj1TrainingGlove_down(i,:) = decimate(subj1TrainingGlove(i,:), r);
