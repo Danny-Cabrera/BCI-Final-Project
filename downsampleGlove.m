169d63bdcfe6c6d9efb96122a6c3ea9a33c4763b
spencerpenn <spenn321@gmail.com>
2014-04-29 02:36:33 -0400

    Updated downsampling

--- downsampleGlove.m
+++ downsampleGlove.m
@@ -1,11 +1,14 @@
-% Downsampling dataglove traces
-
 function[subj1TrainingGlove_down] = downsampleGlove (subj1TrainingGlove)
+
+% What it does: Downsampling Glove Data
+% Input: 5 x A matrix of glove data
+% Output: 5 x A/50 matrix of gloves data (downsampled)
 
 lengthData = length(subj1TrainingGlove);
 r = 50;
 subj1TrainingGlove_down = NaN(5,lengthData/r);
 
+% Loops row by row
 for i = 1:5
     subj1TrainingGlove_down(i,:) = decimate(subj1TrainingGlove(i,:), r);
 end
