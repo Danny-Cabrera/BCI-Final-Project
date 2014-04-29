0b195f1ba78436d89602c9531a1310a496d83caf
JoshTycko <joshtycko@gmail.com>
2014-04-29 17:55:56 -0400

    updates for spencer

--- Main_v2.m
+++ Main_v2.m
@@ -14,7 +14,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % LOAD DATA
-importfile('Data.mat')
+
 
 %%
 
@@ -28,7 +28,8 @@ M = dataInputR(subj1TrainingEcog(:,1:NumSamps));
 %%
 % DOWNSAMPLE DATAGLOVE
 
-Y_down = downsampleGlove(subj1TrainingGlove(:,1:NumSamps)); 
+Y_down = downsampleGlove(subj1TrainingGlove(:,1:NumSamps)); %glove should be on same timescale as features
+%for now we have features in windows  50ms apart
 
 %%
 % LINEAR REGRESSION
@@ -41,15 +42,16 @@ R = Rmatrix(M,3);
 % Compute Beta (for each Finger of Glove)
 
 for i = 1:5
-    Y_segment = Y(i,:)';
+    Y_segment = Y_down(i,:)';
     B(:,i) = mldivide(R'*R,R'*Y_segment);
 end
 
+%%
 %Compute Prediction
 
 for i = 1:5
 
-    Y_pred(i,:) = (R*B(:,i))'
+    Y_pred(i,:) = (R*B(:,i))';
 
 end
 
