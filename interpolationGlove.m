089a8597a3eace84e41ef8fc0e0d4aeb5930849e
spencerpenn <spenn321@gmail.com>
2014-04-29 02:26:52 -0400

    Updated Interpol

--- interpolationGlove.m
+++ interpolationGlove.m
@@ -1,9 +1,14 @@
 function[samples_1000hz] = interpolationGlove (samples_20hz)
-
-    x = (1:8000);
-    xx = (1/50:1/50:8000);
     
-    samples_1000hz = NaN(5,400000);
+
+
+    lengthData = length(samples_20hz);
+    
+    dt = 1/50
+    x = (1:lengthData);
+    xx = (dt:dt:lengthData);
+    
+    samples_1000hz = NaN(5,lengthData/dt);
     
     for i = 1:5
 
